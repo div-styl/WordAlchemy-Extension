a9bfcdc1c183d4fdf31dd6479dc782287cc488bb
div-styl <gouhmada@gmail.com>
2023-12-06 11:15:55 +0100

    Update dependencies and add font import and animation 👍

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,4 @@
 // place files you want to import through the `$lib` alias in this folder.
 export { default as Navbar } from './components/header/Navbar.svelte';
 export { default as Searchbar } from './components/body/searchbar.svelte';
+export { default as Cat } from './components/UI/cat.json';
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,29 @@
 <script>
 	import {Searchbar} from '$lib';
+	import { Cat } from '$lib';
+	import { onMount } from 'svelte';
 	let extensiontitle = 'Wordalchemy Extension';
 	let description = 'My Chrome Extension for English Dictionary';
+
+	let lottieElement;
+
+	onMount(() => {
+		import('lottie-web').then((lottie) => {
+			// Load and display the Lottie animation
+			const animation = lottie.loadAnimation({
+				container: lottieElement,
+				loop: true, // Set to true if you want the animation to loop
+				autoplay: true, // Set to true if you want the animation to play automatically
+				animationData: Cat // Replace with your Lottie animation data
+			});
+
+			return () => {
+				// Cleanup when the component is unmounted
+				animation.destroy();
+			};
+		});
+	});
+
 </script>
 
 <svelte:head>
@@ -9,7 +31,8 @@
 	<meta name="description" content={description} />
 </svelte:head>
 
-<section >
+<section>
+	<div bind:this={lottieElement} class="h-[100px]" />
 	<Searchbar />
 </section>
 
